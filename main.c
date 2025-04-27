a86ebba781c7493e23dd1c8d95e64010060d2fb6
TuanTNT19 <tuantnt08@gmail.com>
2025-04-27 20:22:33 +0700

    Basic feature for Led Control App in Pi4 by PC via socket stream

--- main.c
+++ main.c
@@ -3,10 +3,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/socket.h>      
+#include <netinet/in.h>     
+#include <arpa/inet.h>
 
 #define LED_DEVICE_PATH "/dev/my_led23_device"
 
-int fd;
+int ser_fd, dev_fd;
+struct sockaddr_in ser_addr;
+int PC_Port;
+char *PC_IP;
+char *message;
+
+// Signal handler function
+void sig_handler()
+{
+    printf("========= QUICK TURN OFF ========\n");
+    free (message);
+    free (PC_IP);
+    close (ser_fd);
+    close (dev_fd);
+    exit(EXIT_SUCCESS);
+}
 
 // Clear the terminal
 void clrscr() {
@@ -14,46 +33,83 @@ void clrscr() {
     return;
 }
 
-int main() {
-    char *chosen = malloc(10 * sizeof(char));
-    fd = open(LED_DEVICE_PATH, O_RDWR);
-    if (fd < 0) {
-        perror("Failed to open device");
-        return 1;
+int read_func(int fd, char *msg){
+    return read(fd, msg, 20);
+}
+
+int main(){
+    PC_IP = malloc(20);
+    message = malloc(20);
+
+// Signal Ctrl C register
+    if (signal(SIGINT,sig_handler) == SIG_ERR)
+    {
+        printf("Can not handler SIGINT\n");
     }
 
-    while(1){
-        do
-        {
-            printf("1. Led ON\n");
-            printf("0. Led OFF\n");
-            printf("q. Quit Program\n");
-            printf("Enter your chosen: ");
-            fgets(chosen, 10, stdin);
-            if (chosen[0] != '1' && chosen[0] != '0' && chosen[0] != 'q'){
-                printf("Invalid chosen !! Do again \n");
-            }
-        } while (chosen[0] != '1' && chosen[0] != '0' && chosen[0] != 'q');
+    printf("Enter PC IP: ");
+    fflush(stdout);
+    fgets(PC_IP, 20, stdin);
+    PC_IP[strcspn(PC_IP, "\n")] = '\0'; // Xóa \n
+    printf("Enter PC Port: "); 
+    fflush(stdout);
+    scanf("%d", &PC_Port);
+    getchar(); 
 
-        if (chosen[0] == '1'){
-            int ret = write (fd, "1", 1);
-            if (ret == -1)
+    dev_fd = open(LED_DEVICE_PATH, O_RDWR);
+    if (dev_fd < 0) {
+        perror("Failed to open device");
+        sig_handler();
+        return -1;
+    }
+
+    ser_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    ser_addr.sin_port = htons(PC_Port);
+    ser_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, PC_IP, &ser_addr.sin_addr.s_addr);
+
+    int ret = connect(ser_fd, (struct sockaddr *)&ser_addr, sizeof(ser_addr));
+    if (ret  < 0){
+        printf("=== ERROR ===: Can not connect to PC\n");
+        sig_handler();
+        return -1;
+    }
+
+    printf("=========== Connected with PC =========== \n");
+
+    while (1) {
+        if (read_func (ser_fd, message) < 0){
+            printf("=== ERROR ===: Can not read message from PC\n");
+        }
+        if (message[0] == '1'){
+            int re = write (dev_fd, "1", 1);
+            if (re == -1)
             {
-                printf("Can not write 1\n");
+                printf("=== ERROR ===: Can not write 1 to device file\n");
             }
         }
-        else if (chosen[0] == '0'){
-            int ret = write (fd, "0", 1);
-            if (ret == -1)
+        else if (message[0] == '0'){
+            int re = write (dev_fd, "0", 1);
+            if (re == -1)
             {
-                printf("Can not write 0\n");
-            }            
+                printf("=== ERROR ===: Can not write 0 to device file\n");
+            }
+        }
+        else if (message[0] == 'q'){
+            printf ("Disconnect with PC !\n");
+            break;
         }
         else {
             break;
         }
     }
 
-    close (fd);
+    free (message);
+    free (PC_IP);
+    close (ser_fd);
+    close (dev_fd);
+
     return 0;
+
 }
