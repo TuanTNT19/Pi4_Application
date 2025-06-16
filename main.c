c86c95020c89ed211d00ec6c752487ad7e624cf3
TuanTNT19 <tuantnt08@gmail.com>
2025-06-16 20:41:03 +0700

    Upto multi thread to handle multiple connection

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <sys/socket.h>      
 #include <netinet/in.h>     
@@ -12,26 +13,55 @@
 #define CONNECT_TOKEN "Tuan08*"
 #define PR_ERR(str)  printf ("!!! Error in %s function\n", str)
 
+typedef struct client_info{
+    int number;
+    struct sockaddr_in cli_addr;
+} client_info;
+
+client_info *client;
 int ser_fd;
-char *message_r;
+socklen_t len;
 char *token;
+pthread_t *thread_id;
 
 void sig_handler()
 {
     printf("========= QUICK TURN OFF ========\n");
     close (ser_fd);
-    free(message_r);
     free(token);
+    free(thread_id);
+    free(client);
     exit(EXIT_SUCCESS);
 }
 
+static void *mess_handle(void *para)
+{
+    client_info *client = (client_info *)para;
+    int c;
+    int Port;
+    char IP[20];
+    char mess[20];
+    Port = ntohs (client->cli_addr.sin_port);
+    inet_ntop (AF_INET, &client->cli_addr.sin_addr, IP, 20);
+
+    while(1){
+        c = recvfrom(ser_fd, mess, sizeof(mess), 0,
+                         (struct sockaddr *)&client->cli_addr, &len);
+        mess[c] = '\0';
+        printf("***** Client %d at IP %s and Port %d *****\n", client->number, IP, Port);
+        printf ("Message : %s\n", mess);
+        printf ("             *********             \n");
+    }
+}
+
 int main(){
-    struct sockaddr_in ser_addr, cli_addr;
-    socklen_t len = sizeof (struct sockaddr_in);
+    struct sockaddr_in ser_addr;
+    len = sizeof (struct sockaddr_in);
     int ser_port;
-    message_r = malloc (20);
+    int cli_number = 0;
     token = malloc (10);
-    bool ret = false;
+    client = malloc (5 * sizeof(client_info));
+    thread_id = malloc (5 * sizeof(pthread_t));
 
     if (signal(SIGINT,sig_handler) == SIG_ERR)
     {
@@ -57,26 +87,19 @@ int main(){
     }
 
     while (1){
-        int n = recvfrom (ser_fd, token, 20, 0, (struct sockaddr *) &cli_addr, &len);
+        int n = recvfrom (ser_fd, token, 10, 0, (struct sockaddr *)&client[cli_number].cli_addr, &len);
         token[n] = '\0';
         if (!strncmp (CONNECT_TOKEN, token, 7)) {
             int cli_port;
             char cli_ip[20];
-            cli_port = ntohs (cli_addr.sin_port);
-            inet_ntop (AF_INET, &cli_addr.sin_addr.s_addr, cli_ip, 20);
-            printf ("--->Got connection from client at IP: %s - Port: %d \n", cli_ip, cli_port);
-            sendto(ser_fd, "1", 1, 0, (struct sockaddr *)&cli_addr, len);
-            break; 
+            client[cli_number].number = cli_number + 1;
+            cli_port = ntohs (client[cli_number].cli_addr.sin_port);
+            inet_ntop (AF_INET, &client[cli_number].cli_addr.sin_addr.s_addr, cli_ip, 20);
+            printf ("--->Got connection from client %d at IP: %s - Port: %d \n", cli_number + 1, cli_ip, cli_port);
+            sendto(ser_fd, "1", 1, 0, (struct sockaddr *)&client[cli_number].cli_addr, len);
+            pthread_create(&thread_id[cli_number], NULL, &mess_handle, &client[cli_number]);
+            cli_number ++;
         }
-        else {
-            printf ("!!! Wrong Token ---> Can not connect to client\n");
-            sendto(ser_fd, "0", 1, 0, (struct sockaddr *)&cli_addr, len);
-        }
-    }
-
-    while (1){
-        recvfrom (ser_fd, message_r, 20, 0, (struct sockaddr *) &cli_addr, &len);
-        printf ("Message Receive from client : %s\n", message_r);
     }
 
     return 0;
