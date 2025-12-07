445f6a1a489333b6a6bf7cc3d014ad4836266c5f
TuanTNT19 <tuantnt08@gmail.com>
2025-12-07 16:43:26 +0700

    test 7/12 6

--- dhcp_server.c
+++ dhcp_server.c
@@ -171,32 +171,9 @@ int send_dhcp_reply(pcap_t *handle, const dhcp_packet *req,
     udp->uh_ulen = htons(udp_len);
     ip->tot_len = htons(ip_len);
 
-    // // /******************** IP CHECKSUM ********************/
-    // // ip->check = 0;
-    // // ip->check = checksum((uint16_t*)ip, sizeof(struct iphdr)/2);
-
-    // /******************** UDP CHECKSUM ********************/
-    // struct {
-    //     uint32_t src, dst;
-    //     uint8_t zero;
-    //     uint8_t proto;
-    //     uint16_t len;
-    // } pseudo;
-
-    // pseudo.src = ip->saddr;
-    // pseudo.dst = ip->daddr;
-    // pseudo.zero = 0;
-    // pseudo.proto = IPPROTO_UDP;
-    // pseudo.len = udp->uh_ulen;
-
-    // int pseudo_len = sizeof(pseudo) + udp_len;
-    // uint8_t *pseudo_buf = malloc(pseudo_len);
-
-    // memcpy(pseudo_buf, &pseudo, sizeof(pseudo));
-    // memcpy(pseudo_buf + sizeof(pseudo), udp, udp_len);
-
-    // udp->uh_sum = checksum((uint16_t*)pseudo_buf, pseudo_len/2);
-    // free(pseudo_buf);
+    /******************** IP CHECKSUM ********************/
+    ip->check = 0;
+    ip->check = checksum((uint16_t*)ip, sizeof(struct iphdr)/2);
 
     return pcap_sendpacket(handle, buffer, total_len);
 }
